42cd032058b74e784c74e478ab34717c985be8cd
TanmayeeThiriveedhi <meet.tanmayee@gmail.com>
2025-11-17 18:42:23 +0530

    Add Firebase SDK imports and initialization
    Import Firebase SDK and initialize app with configuration.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCKGeAifnGzUXUcYPyq66QZsFw0H2KzYCQ",
+    authDomain: "student-management-183.firebaseapp.com",
+    projectId: "student-management-183",
+    storageBucket: "student-management-183.firebasestorage.app",
+    messagingSenderId: "278398940942",
+    appId: "1:278398940942:web:592dace5f70ac04112c95f",
+    measurementId: "G-CDCQVZ4FFN"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
 /* Final local-only app implementing requested changes */
 (function(){
   const $ = id => document.getElementById(id);
